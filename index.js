7c7d81d4288255b47fdd14a5837eb9536212c846
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-02-10 14:42:39 -0800

    POST user with name and bio

--- index.js
+++ index.js
@@ -19,6 +19,34 @@ server.get('/api/users', (req, res) => {
     res.status(200).json(users);
   }).catch(err => {
     console.log(err);    
-    res.status(500).json({ errorMessage: "The users information could not be retrieved." });
+    res.status(500).json({ errorMessage: "Users information could not be retrieved." });
+  });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  Users.findById(req.params.id).then(user => {
+    if (user.length === 0) {
+      res.status(404).json({ message: "The user with the specified ID does not exist." });
+    } else {
+      res.status(200).json(user);
+    }
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ errorMessage: "The user information could not be retrieved." })
+  });
+});
+
+// add user
+server.post('/api/users', (req, res) => {
+  const newUser = req.body;
+  if (!newUser.name || !newUser.bio ) {
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+  }
+
+  Users.insert(newUser).then(user => {
+    res.status(201).send(user);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ errorMessage: "There was an error while saving the user to the database." })
   });
 });
