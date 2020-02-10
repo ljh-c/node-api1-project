3837426bf6789d1a4fe439d7529211493d5e0b80
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-02-10 13:03:51 -0800

    GET users endpoint

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// implement your API here
+// `npm i express`
+// `npm run server` to start API
+
+const express = require('express');
+
+const Users = require('./data/db.js');
+
+const server = express();
+
+const port = 5000;
+server.listen(port, console.log(`API on port ${port}`));
+
+// so express reads JSON from body
+server.use(express.json());
+
+// return list of users
+server.get('/api/users', (req, res) => {
+  Users.find().then(users => {
+    res.status(200).json(users);
+  }).catch(err => {
+    console.log(err);    
+    res.status(500).json({ errorMessage: "The users information could not be retrieved." });
+  });
+});
